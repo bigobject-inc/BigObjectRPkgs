ad286dfca65b68aec6a249f438bb4d70a69ddb0c
Wush Wu <wush978@gmail.com>
2015-09-07 13:12:43 +0800

    implement documentation of bigobject_connection and bigobject_sql

--- RBigObject/R/bigobject_sql.R
+++ RBigObject/R/bigobject_sql.R
@@ -1,3 +1,9 @@
+#'@title Configure the default connection to BigObject
+#'@param ip string. The ip address or domain name to the BigObject instance.
+#'@param port string. The port number.
+#'@details
+#'The default ip is stored in the options with key: \code{"BIGOBJECT_IP"}.
+#'The default port is stored in the options with key: \code{"BIGOBJECT_PORT"}.
 #'@export
 bigobject_connection <- function(ip, port) {
   stopifnot(length(ip) == 1)
@@ -5,6 +11,19 @@ bigobject_connection <- function(ip, port) {
   options("BIGOBJECT_IP" = as.character(ip)[1], "BIGOBJECT_PORT" = as.character(port)[1])
 }
 
+#'@title Submit SQL Query to BigObject
+#'@param stmt character. A series of SQL statement. 
+#'Each element is a complete SQL statement.
+#'These query will be executed from the first element to the last element.
+#'@param ip string. The ip address or domain name to the BigObject instance.
+#'@param port string. The port number.
+#'@return A list with the following slots:
+#'\itemize{
+#'  \item{"Content"} The returned content.
+#'  \item{"Status"}
+#'  \item{"Err"}
+#'}
+#'@references \url{http://docs.bigobject.io/API/index.html}
 #'@export
 #'@importFrom httr POST
 #'@importFrom httr content
